809d499cac32cb72c75de9cd0b2d4d7d8319b7b1
wangpeng <565879340@qq.com>
2021-07-27 14:48:49 +0800

    添加代码

---
+++ Example/Pods/Target Support Files/Pods-WPCategoryKit_Tests/Pods-WPCategoryKit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WPCategoryKit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_WPCategoryKit_Tests
+@end
---
+++ Example/Pods/Target Support Files/WPCategoryKit/WPCategoryKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_WPCategoryKit : NSObject
+@end
+@implementation PodsDummy_WPCategoryKit
+@end
